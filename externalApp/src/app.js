d79c179ebf6d02e7ab9efb0f064cfead0f367f53
cbasler <clemens.basler@1und1.de>
2016-07-11 13:43:54 +0200

    have 2 localserver now

--- externalApp/src/app.js
+++ externalApp/src/app.js
@@ -2,6 +2,7 @@ import 'babel-polyfill';
 import cats from './cats';
 import $ from 'jquery';
 import angular from 'angular';
+// require('file?name=[name].[ext]!../index.html');
 
  $('<h1>Cats</h1>').appendTo('body');
  const ul = $('<ul></ul>').appendTo('body');

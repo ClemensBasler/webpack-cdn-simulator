d7ca6ebdae0af6e4127520d5893f2eb8afd9fc6a
cbasler <clemens.basler@1und1.de>
2016-07-12 17:14:28 +0200

    added angular

--- externalApp/src/app.js
+++ externalApp/src/app.js
@@ -1,6 +1,4 @@
 require('file?name=[name].[ext]!../index.html');
 var angular = require('angular');
 
-var value = "20.10.2016"
-
-console.log(angular.isDate(value));
+console.log(angular.equals('1', '1'));

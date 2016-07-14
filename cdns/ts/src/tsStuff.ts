1b05260c51043c003adeea454c086aa39955a85c
clemensbasler <clemens-basler@t-online.de>
2016-07-14 17:27:19 +0200

    adding angular dep to ts cdn

--- cdns/ts/src/tsStuff.ts
+++ cdns/ts/src/tsStuff.ts
@@ -1,6 +1,8 @@
 import greeter = require('./greeter');
 import $ = require('jquery');
+import angular = require('angular');
 
 $(() => {
   $(document.body).html(greeter('from the ts cdn :)'));
 });
+console.log(angular.equals('1', '1'));
